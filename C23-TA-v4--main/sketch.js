b6fa99d98c68cf756f5f3103e30dcc751e252ad8
joana maria de jesus souza <98822804+JoanaMariaDeJesusSouza1@users.noreply.github.com>
2023-04-20 20:38:14 -0300

    Add files via upload

---
+++ C23-TA-v4--main/sketch.js
@@ -0,0 +1,21 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var engine,world;
+
+function setup() {
+    createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+
+   
+}
+
+function draw() {
+    background(51);
+    
+}
+
